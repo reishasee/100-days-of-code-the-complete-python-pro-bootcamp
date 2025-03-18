e4033183bbcf9bc24f927851bea1462deff8a72e
reishasee <131441276+reishasee@users.noreply.github.com>
2025-03-18 20:29:12 +0800

    Create data.py

---
+++ Day-17/data.py
@@ -0,0 +1,402 @@
+question_data = [
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Vehicles",
+        "question": "In 1993 Swedish car manufacturer Saab experimented with replacing the steering wheel with a joystick in a Saab 9000.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "General Knowledge",
+        "question": "The vapor produced by e-cigarettes is actually water.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science &amp; Nature",
+        "question": "Igneous rocks are formed by excessive heat and pressure.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime &amp; Manga",
+        "question": "Studio Ghibli is a Japanese animation studio responsible for the films &quot;Wolf Children&quot; and &quot;The Boy and the Beast&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Japanese Anime &amp; Manga",
+        "question": "The anime Attack on Titan was directed by Tetsur\u014d Araki, the same person who directed the anime Highschool of the Dead.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Geography",
+        "question": "San Marino is the only country completely surrounded by another country.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science &amp; Nature",
+        "question": "Salt is 100% composed of Sodium.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "Bulls are attracted to the color red.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Video Games",
+        "question": "In Team Fortress 2, being disguised as a scout or medic results in a speed boost.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "History",
+        "question": "United States President John F. Kennedy was assassinated during his presidential motorcade in Atlanta, Georgia on November 22nd, 1963.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "Video streaming website YouTube was purchased in it's entirety by Facebook for US$1.65 billion in stock.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "Gumbo is a stew that originated in Louisiana.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "History",
+        "question": "Adolf Hitler was tried at the Nuremberg trials.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Cartoon &amp; Animations",
+        "question": "Night on Bald Mountain was one of the musical pieces featured in Disney's 1940's film Fantasia.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Film",
+        "question": "Joan Cusack starred in the 2009 disaster movie, '2012.'",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Video Games",
+        "question": "In Kingdom Hearts the Paopu fruit is said to intertwine the destinies for two people forever.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "A pasodoble is a type of Italian pasta sauce.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "History",
+        "question": "Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke was to complain about a draft in the chambers.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "Slovakia is a member of European Union-",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "General Knowledge",
+        "question": "SCP-173 was the first SCP article written for the web-based collaborative fiction project known as the 'SCP Foundation'",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime &amp; Manga",
+        "question": "In Chobits, Hideki found Chii in his apartment.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "History",
+        "question": "The fourth funnel of the RMS Titanic was fake designed to make the ship look more powerful and symmetrical.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "The video game 'Fuel' has an open world that is 5,560 square miles?",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Vehicles",
+        "question": "Ferrari has never made a V10 engine for any of its cars.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Video Games",
+        "question": "The main antagonist in the video game Saints Row IV is an entity known as The Mastermind.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Video Games",
+        "question": "TF2: The Heavy's voice actor, Gary Schwartz, voices the Demoman as well ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Animals",
+        "question": "A flock of crows is known as a homicide.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Mythology",
+        "question": "In the Greek Mythology, the Gorgon can turn those who behold her to stone.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Film",
+        "question": "In the original script of 'The Matrix', the machines used humans as additional computing power instead of batteries.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Animals",
+        "question": "The Killer Whale is considered a type of dolphin.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, and Chrome OS.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Art",
+        "question": "Pablo Picasso is one of the founding fathers of 'Cubism.'",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Geography",
+        "question": "Norway has a larger land area than Sweden.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Geography",
+        "question": "Liechtenstein does not have an airport.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Science &amp; Nature",
+        "question": "An atom contains a nucleus.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science &amp; Nature",
+        "question": "The 'Gympie Stinger' is the deadliest plant in the world.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Geography",
+        "question": "Nova Scotia is on the east coast of Canada.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Video Games",
+        "question": "In Until Dawn, both characters Sam and Mike cannot be killed under any means until the final chapter of the game.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Comics",
+        "question": "In the webcomic Homestuck, the first character introduced is Dave Strider.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Politics",
+        "question": "Joko Widodo has appeared in the cover of a TIME magazine.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "John Williams composed the music for 'Star Wars'.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Cartoon &amp; Animations",
+        "question": "In the 'Shrek' film franchise, Donkey is played by Eddie Murphy.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Music",
+        "question": "Musical artist, Future, collaborated with Kendrick Lamar for the song: 'Mask Off'.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "General Knowledge",
+        "question": "All strawberries are red.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Geography",
+        "question": "Ottawa is the capital of Canada.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "History",
+        "question": "Abraham Lincoln was the first U.S. President to be born outside the borders of the thirteen original states. ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "The first dual-core CPU was the Intel Pentium D.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Video Games",
+        "question": "In Terraria, you can craft the Cell Phone pre-hardmode.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Video Games",
+        "question": "Nintendo started out as a playing card manufacturer.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
