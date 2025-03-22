f1f321fea2cf0202796235379e2d5139a48d601c
reishasee <131441276+reishasee@users.noreply.github.com>
2025-03-23 00:20:57 +0800

    Create main.py

---
+++ Day-22/main.py
@@ -0,0 +1,49 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.tracer(0)
+ball = Ball()
+scoreboard = Scoreboard()
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+screen.listen()
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # needs to bounce
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect left paddle misses:
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
+
